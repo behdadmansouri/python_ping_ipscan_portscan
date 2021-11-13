cea7d62257a86135642281802ea91dd05e8f6986
Behdad Mansouri <behdadaspacey2099@gmail.com>
2021-11-13 20:42:11 +0330

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+# todo get ping from an ip (ping.py result_ping.txt) both ip and name
+# todo scan an ip range and find active hosts (ipscan.py result_ping.txt)
+# todo scan open ports of an active host (portscan.py result_portscan.txt)
+#              test
+# todo ip range 89.43.0.0 to 89.43.7.255 while using a vpn.
+#              phase 2
+# todo nmap, netdiscover, hping3 check results
+# whatweb, httprint, xprub2  get more info about active hosts in that ip area (mention  if other sites used)
+# nmap use: TCP full scan, Stealth scan, UDP scan, Fingerpring scan, Idle scan
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
